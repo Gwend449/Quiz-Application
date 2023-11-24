22fe73146326b3872b79f28884df0fc87740eade
Nikita <nlyapin2013@yandex.ru>
2023-11-24 13:03:07 +0300

    Some Style refactor and test page started

---
+++ chapters/test1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+   <title>Chapter I test</title>
+</head>
+
+<body>
+
+   <div class="container">
+      <h3>Часть I</h3>
+      <div class="row">
+         <form class="row" action="">
+            <div class="form-check">
+               <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+               <label class="form-check-label" for="flexCheckDefault">
+                  Default checkbox
+               </label>
+            </div>
+            <div class="form-check">
+               <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+               <label class="form-check-label" for="flexCheckChecked">
+                  Checked checkbox
+               </label>
+            </div>
+         </form>
+      </div>
+   </div>
+
+</body>
+
+</html>
